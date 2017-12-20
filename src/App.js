f32fbdfacfe34643653ad1dfe027ff21b8e93d11
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2017-12-21 00:22:32 +1100

    Add basic Button component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button/Button";
 import Display from "./Display/Display";
 
 const App = () => (
@@ -6,6 +7,12 @@ const App = () => (
     <Display text="1502°C" />
     <Display text="01:23" />
     <Display text="idle" />
+    <Button
+      text="Start"
+      onClick={() => console.log("Start button activated")}
+    />
+    <Button text="Up" onClick={() => console.log("Up button activated")} />
+    <Button text="Down" onClick={() => console.log("Down button activated")} />
   </div>
 );
 
