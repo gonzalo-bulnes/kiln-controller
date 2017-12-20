d0729e24d130ad71953eefafff1f737285de10dc
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2017-12-20 23:58:26 +1100

    Remove example code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Display from "./Display/Display";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Display text="1502°C" />
-        <Display text="01:23" />
-        <Display text="idle" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Display text="1502°C" />
+    <Display text="01:23" />
+    <Display text="idle" />
+  </div>
+);
 
 export default App;
