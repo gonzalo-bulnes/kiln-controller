46633b87d8090d13fe2714e15bfa880d5eb0e3b5
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2017-12-26 09:17:09 +1100

    Fix add missing store Provider to App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import storeFactory from "./store";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe("App", () => {
+  let store;
+
+  beforeAll(() => {
+    store = storeFactory({});
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    );
+  });
 });
